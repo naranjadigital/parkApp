80fd34dd325b066838da0bffad1acc5ee578207b
megagrinch <miguel_trinch@hotmail.com>
2018-12-13 21:04:57 -0500

    produccion

--- parkApp/prod.py
+++ parkApp/prod.py
@@ -2,5 +2,10 @@ from parkApp.base import *
 
 ALLOWED_HOSTS = ['192.168.1.12']
 
-BASE_DIR = '/home/deploy/parkApp/parkApp'
+BASE_DIR = '/home/deploy/parkApp/parkApp/'
 STATIC_ROOT = "/home/deploy/parkApp/static"
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
